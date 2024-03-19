8a7b20ae4f909ade6d4aaa9fb7d06e2cc2927b93
ForSerious <forxxserious@gmail.com>
2024-03-19 07:09:18 -0600

    CRF 12 and maybe a fix for vpy.

--- FullVideoRun.py
+++ FullVideoRun.py
@@ -32,7 +32,8 @@ TVAINUM = 1
 # How many TVAI APO instances
 APONUM = 1
 PREDIR = 'D:\\'
-FOLDERS = 'G:\\Vid\\FullRunList.txt'
+FOLDERS = 'C:\\PiStuff\\FullRunList.txt'
+OTHER_DRIVE = 'S:\\'
 
 
 def generate_next_file(rootdir):
@@ -122,7 +123,7 @@ def ff_pass(filepath):
             generate_vpy(filepath, filepath['-file'])
             command = FFMPEG + ' -hide_banner -stats_period 2.0 -nostdin -f vapoursynth -i "' + os.path.join(get_drive_path(filepath['-path'], filepath, True), filepath['-file'] +
             '.vpy') + '" -y ' + ss + t + '-map 0:v -map 0:a? -map 0:s? -map 0:d? -map 0:t? ' + decimate + '-c:v libx265 -crf' \
-            ' 12 -preset slow "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + '.mkv') + '"'
+            ' 12 -preset veryfast "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + '.mkv') + '"'
         if filepath.get('-vpy', null) == null and filepath.get('-deinterlace', null) != null:
             #if not os.path.exists(os.path.join(out_path[0], out_path[1])):
             #    os.mkdir(os.path.join(out_path[0], out_path[1]))
@@ -132,19 +133,19 @@ def ff_pass(filepath):
                 decimate = ',"decimate=' + decimate_tmp + '" '
             command = FFMPEG + ' -hide_banner -stats_period 2.0 -nostdin -y -i "' + os.path.join(get_drive_path(filepath['-path'], filepath, True),
             filepath['-file'] + '.' + filepath['-ext']) + '" ' + ss + t + '-r ' + filepath['-r'] + ' -filter_complex setsar=1,bwdif=mode=1:parity=-1:deint=0' + decimate + '-c:v libx265 -crf' \
-            ' 12 -preset slow "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + '.mkv') + '"'
+            ' 12 -preset veryfast "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + '.mkv') + '"'
             #now_tif = True
         if filepath.get('-deinterlace', null) == null:
             command = FFMPEG + ' -hide_banner -stats_period 2.0 -nostdin -i "' + os.path.join(get_drive_path(filepath['-path'], filepath, True),
                       filepath['-file'] + '.' + filepath['-ext']) + '" -y ' + ss + t + '-map 0:v -map 0:a? -map 0:s? -map 0:d? -map 0:t? ' + yadif + \
-                      r + dering + '-c:v libx265 -crf 12 -preset slow -c:a copy -c:s copy -max_muxing_queue_size 4096 "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] +
+                      r + dering + '-c:v libx265 -crf 12 -preset veryfast -c:a copy -c:s copy -max_muxing_queue_size 4096 "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] +
                       '.mkv') + '"'
             if filepath.get('-vpy', null) != null:
                 generate_vpy(filepath, out_path[1])
                 run_three = True
                 command3 = FFMPEG + ' -hide_banner -stats_period 2.0 -nostdin -f vapoursynth -i "' + os.path.join(get_drive_path(filepath['-path'], filepath, True), out_path[1] +
                 '.vpy') + '" -y -map 0:v -map 0:a? -map 0:s? -map 0:d? -map 0:t? ' + r + decimate + '-c:v libx265 -crf' \
-                ' 12 -preset slow "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + 'vpy' + '.mkv') + '"'
+                ' 12 -preset veryfast "' + os.path.join(get_drive_path(out_path[0], filepath, False), out_path[1] + 'vpy' + '.mkv') + '"'
                 out_path = (filepath['-path'], out_path[1] + 'vpy')
                 filepath['-out'] = out_path[1]
         if filepath.get('-name', null) != null:
@@ -170,6 +171,8 @@ def ff_pass(filepath):
         if get_json_state(filepath).get('ff1', null) == null:
             subprocess.call(command)
             save_json_state(filepath, 'ff1')
+            if filepath.get('-vpy', null) != null:
+                filepath = swap_drive_path(filepath)
         if run_three and get_json_state(filepath).get('ff2', null) == null:
             if DEBUG:
                 print('The Command3: ')
