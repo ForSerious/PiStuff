aaf79e961cd7538ca4ceb04c6f07df5fe42eb2cc
ForSerious <forxxserious@gmail.com>
2024-07-05 14:29:58 -0600

    Removed the dot at the end of the time.

--- AudioGrip/StreamLine.py
+++ AudioGrip/StreamLine.py
@@ -268,7 +268,7 @@ def seconds_to_str(elapsed=None):
     if elapsed is None:
         return strftime("%Y-%m-%d %H:%M:%S", localtime())
     else:
-        return str(timedelta(seconds=elapsed))[:-6]
+        return str(timedelta(seconds=elapsed))[:-7]
 
 
 if __name__ == '__main__':
